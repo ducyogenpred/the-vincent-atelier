3afdf3456f016ec51aa2daf0e1c0a4c7f7b631a5
ducyogenpred <ducyogenpred@gmail.com>
2025-05-17 21:21:18 +0800

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,23 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import fg from "fast-glob";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const htmlFiles = fg.sync(["**/*.html", "!node_modules/**", "!dist/**"]);
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: Object.fromEntries(
+        htmlFiles.map((file) => {
+          const fileName = file.split("/").pop();
+          return [fileName.replace(".html", ""), resolve(__dirname, file)];
+        }),
+      ),
+    },
+  },
 });
